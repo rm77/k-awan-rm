751935a368781214ba5592136b28f7b55ca02eb8
roy <royyana@gmail.com>
2022-12-07 13:14:35 +0700

    dashboard

--- containers/kubernetes/kind/dashboard.sh
+++ containers/kubernetes/kind/dashboard.sh
@@ -1,1 +1,3 @@
-octant --kubeconfig $(pwd)/kubeconfig --disable-open-browser --disable-origin-check  --listener-addr :22222 --accepted-hosts 0.0.0.0 2> /dev/null  &
+./octant --kubeconfig $(pwd)/kubeconfig --disable-open-browser --disable-origin-check  --listener-addr :22222 --accepted-hosts 0.0.0.0 
+
+# /dev/null  &
